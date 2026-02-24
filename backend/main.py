71252e6a3783467f60e6dbe4d392f6ae91130e6e
Ubuntu <ubuntu@ip-172-31-32-225.ap-south-1.compute.internal>
2026-02-24 21:31:18 +0000

    Integrated PostgreSQL backend

--- backend/main.py
+++ backend/main.py
@@ -1,8 +1,25 @@
 from fastapi import FastAPI
-from sqlalchemy import create_engine
+from sqlalchemy import create_engine, text
+import os
+import time
 
 app = FastAPI()
 
+DATABASE_URL = os.getenv("DATABASE_URL")
+
+for i in range(10):
+    try:
+        engine = create_engine(DATABASE_URL)
+        with engine.connect() as conn:
+            conn.execute(text("SELECT 1"))
+        print("Database connected!")
+        break
+    except Exception:
+        print("Waiting for database...")
+        time.sleep(3)
+
 @app.get("/")
 def read_root():
-    return {"message": "FastAPI Backend Working!"}
+    with engine.connect() as conn:
+        conn.execute(text("SELECT 1"))
+    return {"message": "Backend + PostgreSQL Working 🚀"}
